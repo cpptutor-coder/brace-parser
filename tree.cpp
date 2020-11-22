6cf309fb319cce5b45329768a20fedaddd86797d
Vikas <vikas@liquidsky.com>
2020-11-21 22:54:15 -0500

    Almost finalized version

--- tree.cpp
+++ tree.cpp
@@ -8,9 +8,11 @@ namespace
     vector<string> combine(vector<string> vec1, vector<string> vec2)
     {
         vector<string> tokens;
-        for(auto& v1 : vec1) {
-            for(auto& v2: vec2) {
-                tokens.push_back(v1+v2);
+        for (auto &v1 : vec1)
+        {
+            for (auto &v2 : vec2)
+            {
+                tokens.push_back(v1 + v2);
             }
         }
         return tokens;
@@ -18,7 +20,8 @@ namespace
 
     void dump_vector(vector<string> vec)
     {
-        for(auto& v: vec) {
+        for (auto &v : vec)
+        {
             cout << v << " ";
         }
         cout << endl;
@@ -26,114 +29,114 @@ namespace
 
 } // namespace
 
-void Node::add_data(char c)
-    {
-        data_.push_back(c);
-    }
+void Node::add_label(char c)
+{
+    label_.push_back(c);
+}
 
-    string Node::get_data() const
-    {
-        return data_;
-    }
-    
-    shared_ptr<Node> Node::add_child()
-    {
-        auto child = make_shared<Node>();
-        child->set_parent(shared_from_this());
-        children_.push_back(child);
-        return children_.back();
-    }
+string Node::get_label() const
+{
+    return label_;
+}
 
-    shared_ptr<Node> Node::add_sibling()
-    {
-        sibling_ = make_shared<Node>();
-        sibling_->set_parent(get_parent());
-        return sibling_;
-    }
+Node *Node::add_child()
+{
+    children_.push_back(make_unique<Node>(this));
+    return children_.back().get();
+}
 
-    shared_ptr<Node> Node::get_parent() const
-    {
-       return parent_.lock();
-    }
+Node *Node::add_sibling()
+{
+    sibling_ = make_unique<Node>(get_parent());
+    return sibling_.get();
+}
 
-void Node::generate(vector<string>& tokens)
+Node *Node::get_parent() const
+{
+    return parent_;
+}
+
+void Node::generate(vector<string> &tokens)
+{
+    //dump();
+    if (children_.size() > 0)
     {
-        cout << "VIK_DEBUG: generate" << endl;
-        dump();
-        if (children_.size() > 0) {
-            list<vector<string>> child_tokens;
-            for(auto& v: children_) {
-                vector<string> memo;
-                v->generate(memo);
-                assert(memo.size() > 0);
-                dump_vector(memo);
-                child_tokens.push_back(memo);
-            }
-            assert(child_tokens.size() > 0);
-            vector<string> combined = child_tokens.front();
+        list<vector<string>> child_tokens;
+        for (auto &v : children_)
+        {
+            vector<string> memo;
+            v->generate(memo);
+            assert(memo.size() > 0);
+            dump_vector(memo);
+            child_tokens.push_back(memo);
+        }
+        assert(child_tokens.size() > 0);
+        vector<string> combined = child_tokens.front();
+        child_tokens.pop_front();
+        for (auto &v : combined)
+        {
+            v = label_ + v;
+        }
+        while (!child_tokens.empty())
+        {
+            dump_vector(combined);
+            combined = combine(combined, child_tokens.front());
             child_tokens.pop_front();
-            for(auto& v: combined) {
-                v = data_ + v;
-            }
-            while(!child_tokens.empty()) {
-                dump_vector(combined);
-                combined = combine(combined, child_tokens.front());
-                child_tokens.pop_front();
-            }
-            
-            copy(combined.begin(), combined.end(), back_inserter(tokens));
-        }
-        else {
-            assert(!data_.empty());
-            tokens.push_back(data_);
         }
 
-        if (sibling_ != nullptr) {
-            sibling_->generate(tokens);
-        }
+        copy(combined.begin(), combined.end(), back_inserter(tokens));
     }
-
-    void Node::dump() const
+    else
     {
-        cout << (void*)this << " " << data_;
-        for (auto& v: children_) {
-            cout << " Child: " << (void*)v.get() << " " << v->get_data(); 
-        }
-        cout << " Sibling: " << (void*)sibling_.get() << " " << (sibling_ ? sibling_->get_data() : "");
-        auto sp = parent_.lock();
-        cout << " Parent: " << (void*)sp.get() << " " << (sp ? sp->get_data() : "")
-        << endl;
+        assert(!label_.empty());
+        tokens.push_back(label_);
     }
 
-    void Node::set_parent(weak_ptr<Node> p)
+    if (sibling_ != nullptr)
     {
-        parent_ = p;
+        sibling_->generate(tokens);
     }
+}
+
+void Node::dump() const
+{
+    cout << (void *)this << " " << label_;
+    for (auto &v : children_)
+    {
+        cout << " Child: " << (void *)v.get() << " " << v->get_label();
+    }
+    cout << " Sibling: " << (void *)sibling_.get() << " " << (sibling_ ? sibling_->get_label() : "");
+    auto sp = parent_;
+    cout << " Parent: " << (void *)sp << " " << (sp ? sp->get_label() : "")
+         << endl;
+}
 
 void Tree::update_tree(EState state, char c)
 {
     assert(state != EState::invalid);
 
-    switch(state) {
-        case EState::start:
-        case EState::alpha:
-            curr_node_->add_data(c);
+    switch (state)
+    {
+    case EState::start:
+    case EState::alpha:
+        curr_node_->add_label(c);
         break;
-        case EState::open_brace:
-            curr_node_ = curr_node_->add_child();
+    case EState::open_brace:
+        curr_node_ = curr_node_->add_child();
         break;
-        case EState::close_brace:
-            curr_node_ = curr_node_->get_parent();
-            if (curr_node_ == nullptr) {
-                throw runtime_error("Parsing error. One or more extra \"}\" ");
-            }
+    case EState::close_brace:
+        curr_node_ = curr_node_->get_parent();
+        if (curr_node_ == nullptr)
+        {
+            throw runtime_error("Parsing error. One or more extra \"}\" ");
+        }
         break;
-        case EState::comma:
-            curr_node_ = curr_node_->add_sibling();
+    case EState::comma:
+        curr_node_ = curr_node_->add_sibling();
         break;
-        default:
-            assert(false);
-            throw logic_error("Unknown EState");
+    default:
+        assert(false);
+        throw logic_error("Unknown EState");
     }
 }
 
@@ -146,5 +149,5 @@ vector<string> Tree::generate() const
 
 bool Tree::is_valid() const
 {
-    return (curr_node_ == root_node_);
+    return (curr_node_ == root_node_.get());
 }
--- tree.h
+++ tree.h
@@ -7,27 +7,23 @@
 #include <vector>
 #include <exception>
 
-class Node : public std::enable_shared_from_this<Node>
+class Node
 {
   public:
-  // VIK_TODO: Find out how to set the parent from constructor itself
-    //explicit Node(Node* n = nullptr) : parent_(n) {}
-    void add_data(char c); // VIK_TODO: See if we can send in constructor
-    std::string get_data() const; // VIK_TODO: Maybe change name to something better or at least get_label
-    std::shared_ptr<Node> add_child();
-    std::shared_ptr<Node> add_sibling();
-    std::shared_ptr<Node> get_parent() const;
+    explicit Node(Node* n = nullptr) : parent_(n) {}
+    void add_label(char c);
+    std::string get_label() const;
+    Node* add_child();
+    Node* add_sibling();
+    Node* get_parent() const;
     void generate(std::vector<std::string>& tokens);
     void dump() const;
+  
   private:
-  // VIK_TODO: Find how not to need it and set in constructor itself
-    void set_parent(std::weak_ptr<Node> p);
-
-    std::string data_;
-    // VIK_TODO: Make them unique_ptr again later on
-    std::vector<std::shared_ptr<Node>> children_;
-    std::shared_ptr<Node> sibling_;
-    std::weak_ptr<Node> parent_;
+    std::string label_;
+    std::vector<std::unique_ptr<Node>> children_;
+    std::unique_ptr<Node> sibling_;
+    Node* parent_;
 };
 
 class Tree
@@ -37,8 +33,8 @@ class Tree
       std::vector<std::string> generate() const;
       bool is_valid() const;
   private:
-    std::shared_ptr<Node> root_node_ = std::make_shared<Node>();
-    std::shared_ptr<Node> curr_node_ = root_node_;
+    std::unique_ptr<Node> root_node_ = std::make_unique<Node>();
+    Node* curr_node_ = root_node_.get();
 };
 
 #endif
